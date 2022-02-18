569f874fd0381a0270b95fbc6067284be24dfd58
ChuckWagon2480 <ghwar100@gmail.com>
2022-02-18 15:40:08 +0900

    [Baekjoon] 1026

---
+++ Baekjoon/1026.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+a.sort()
+b.sort(reverse=True)
+
+ans = 0
+for i in range(n):
+  ans += a[i]*b[i]
+
+print (ans)
